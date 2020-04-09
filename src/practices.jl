1687706d74ea1d9f0585bd05f17d3384eedf56ad
Yueh-Hua Tu <a504082002@gmail.com>
2020-04-09 13:54:48 +0800

    Files generated by PkgTemplates

---
+++ src/practices.jl
@@ -0,0 +1,5 @@
+module practices
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using practices
+using Test
+
+@testset "practices.jl" begin
+    # Write your own tests here.
+end
