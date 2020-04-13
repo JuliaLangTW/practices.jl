cf69f079fe489b62bd7fa78fcc1fb53cabe494f7
CurryH <50604928+nuclear718@users.noreply.github.com>
2020-04-13 22:00:27 +0800

    Nuclear718_bb
    Fit the format!

---
+++ BubbleSort/src/Nuclear718_bb.jl
@@ -0,0 +1,29 @@
+module Nuclear718_bb
+
+function nuclear718_bbsort!(x::Vector=rand(1:1:100,100))
+while true
+a=0;    
+for k in 1:1:length(x)-1
+    v1=x[k]
+    v2=x[k+1]
+    if v1<v2
+    x[k]=v2
+    x[k+1]=v1        
+    a=a+1;
+    else
+    end
+end
+if a==0
+break
+end
+end
+return x
+end
+
+export Nuclear718_bb
+
+end # module
+
+
+
+
